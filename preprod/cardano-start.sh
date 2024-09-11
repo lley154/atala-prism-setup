a90633815e069d4b5f319b0ae86e845dcc5aab88
tienna <tienna@gmail.com>
2024-09-11 11:00:40 +0700

    Update cardano-node by 9.1.1 and config files

--- preprod/cardano-start.sh
+++ preprod/cardano-start.sh
@@ -10,18 +10,18 @@ if [ "$(ls -A "${NODE_DB}")" ]
 then
     echo "Node state is present, not downloading the snapshot."
 else
-    if [ $CARDANO_NETWORK == "mainnet" ]
+    if [ $NETWORK == "mainnet" ]
     then
         # download the node-db snapshot, or wait for the node to sync for a long time
         curl -o - https://downloads.csnapshots.io/mainnet/$(curl -s https://downloads.csnapshots.io/mainnet/mainnet-db-snapshot.json| jq -r .[].file_name ) | lz4 -c -d - | tar -x -C $NODE_DB
         mv $NODE_DB/db/* $NODE_DB/
         rm -rf $NODE_DB/db
-    elif [ $CARDANO_NETWORK == "preprod" ]
+    elif [ $NETWORK == "preprod" ]
     then
         curl -o - https://downloads.csnapshots.io/testnet/$(curl -s https://downloads.csnapshots.io/testnet/testnet-db-snapshot.json| jq -r .[].file_name ) | lz4 -c -d - | tar -x -C $NODE_DB
         mv $NODE_DB/db/* $NODE_DB/
         rm -rf $NODE_DB/db
-    elif [ $CARDANO_NETWORK == "sanchonet" ]
+    elif [ $NETWORK == "sanchonet" ]
     then echo "no cache for sancho";
     else
         echo "NETWORK must be mainnet or preprod or sanchonet"
@@ -30,4 +30,4 @@ else
 fi
 
 # start the services
-NETWORK=${NETWORK} NODE_DB=${NODE_DB} docker-compose -p cardano -f ./docker-compose-cardano.yml --env-file .env-cardano up -d
+NETWORK=${NETWORK} NODE_DB=${NODE_DB} docker compose -p cardano -f ./docker-compose-cardano.yml --env-file .env-cardano up -d
